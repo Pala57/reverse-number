fdf0f1c1133498de957c271444cf5d921dcfe3ce
Олександр Паламарчук <a.palamar4uck@gmail.com>
2020-07-23 11:02:12 +0300

    Add project files.

---
+++ task3/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Math;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Globalization.CultureInfo customCulture = (System.Globalization.CultureInfo)
+                      System.Threading.Thread.CurrentThread.CurrentCulture.Clone();
+            customCulture.NumberFormat.NumberDecimalSeparator = ".";
+            System.Threading.Thread.CurrentThread.CurrentCulture = customCulture;
+            Console.OutputEncoding = Encoding.Unicode;
+            Console.InputEncoding = Encoding.Unicode;
+            int N;
+
+            int m = 0;
+
+            bool f;
+            do
+            {
+                Console.WriteLine("Ввести N= ");
+                f = int.TryParse(Console.ReadLine(), out N);
+                if (!f)
+                {
+                    Console.WriteLine("Помилка введіть ще раз");
+                }
+            } while (!f);
+
+            while (N != 0)
+            {
+                m *= 10;
+                m += N % 10;
+                N /= 10;
+            }
+            Console.WriteLine(m);
+
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
